22be1d74b74c713d507e62cab28843bd8be218ce
max sharples <max.sharples@impactdata.com.au>
2013-10-11 18:46:23 +1100

    two basic functions

--- lib/math.ex
+++ lib/math.ex
@@ -6,4 +6,34 @@ defmodule Math do
   def start(_type, _args) do
     Math.Supervisor.start_link
   end
+
+  @moduledoc """
+  Provides som helpful mathematical functions
+  """
+  
+  @doc """
+  Sum the contents of a list
+
+  ## Examples
+
+    iex> Math.sum([1,2,3])
+    6
+  """
+  def sum(list) do
+    List.foldl(list, 0, fn (x, acc) -> x + acc end)
+  end
+
+  @doc """
+  Calculate the mean from a list of numbers
+
+  ## Examples
+    
+    iex> Math.mean([1,2,3])
+    2
+  """
+  def mean(list) do
+    sum(list) / Enum.count(list)
+  end
+
+
 end
--- test/math_test.exs
+++ test/math_test.exs
@@ -1,7 +1,13 @@
 defmodule MathTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert(true)
+  test "sum a list" do
+    assert Math.sum([1,2,3,4,5,6,7,8,9]) == 45
   end
+
+  test "calculate mean" do
+    assert Math.mean([1]) == 1
+    assert Math.mean([1,2,3,4,5,6,7,8,9]) == 5
+  end
+
 end
