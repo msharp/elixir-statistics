f717a83db5d0169cc848763897e3070f54116812
max sharples <max.sharples@impactdata.com.au>
2013-10-11 20:14:00 +1100

    added median function

--- lib/math.ex
+++ lib/math.ex
@@ -35,5 +35,51 @@ defmodule Math do
     sum(list) / Enum.count(list)
   end
 
+  @doc """
+  Get the median value from a list
+
+  ## Examples
+  
+    iex> Math.median([1,2,3])
+    2
+    iex> Math.median([1,2,3,4])
+    2.5
+
+  """
+  def median(list) do
+    sorted = :lists.sort(list)
+    middle = (Enum.count(list) - 1) / 2
+    median(sorted, middle, 0, [])
+  end
+  # for when we haven't reached the mid-point(s)
+  defp median(list, middle, pos, acc) when pos < :erlang.trunc(middle) do
+    [_|tail] = list
+    pos = pos + 1
+    median(tail, middle, pos, acc)
+  end
+  # for the case when there is a definite middle point
+  defp median(list, middle, pos, _) when pos == middle do
+    [head|_] = list
+    head
+  end
+  # we've passed the midpoint for a two-item median
+  defp median(list, middle, pos, acc) when pos > middle do
+    [head|_] = list
+    mean([head|acc])
+  end
+  # we've reached the first of the two middle values
+  defp median(list, middle, pos, _) when pos == :erlang.trunc(middle) do
+    [head|tail] = list
+    pos = pos + 1
+    median(tail, middle, pos, [head])
+  end
+  
+
+  @doc """
+  Get square root from Erlang
+  """
+  def sqrt(num) do
+    :math.sqrt(num)
+  end
 
 end
--- test/math_test.exs
+++ test/math_test.exs
@@ -10,4 +10,13 @@ defmodule MathTest do
     assert Math.mean([1,2,3,4,5,6,7,8,9]) == 5
   end
 
+  test "calculate median" do
+    assert Math.median([1,2,3,4,5,6,7,8,9]) == 5
+    assert Math.median([1,2,3,4,5,6,7,8]) == 4.5
+  end
+
+  test "calculate square root" do
+    assert Math.sqrt(64) == 8
+  end
+
 end
