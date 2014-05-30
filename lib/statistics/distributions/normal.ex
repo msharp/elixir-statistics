f31b3187de755d67690b3ac0ebbe28da0b797579
Max Sharples <maxsharples@gmail.com>
2014-05-30 11:46:19 +1000

    placeholder module and test

---
+++ lib/statistics/distributions/normal.ex
@@ -0,0 +1,19 @@
+defmodule Statistics.Distributions.Normal do
+  #use Application.Behaviour
+
+  import Statistics.MathHelpers
+  alias Statistics.MathHelpers, as: M
+
+  def pmf do
+    0.0
+  end
+
+  def cdf do
+    0.0
+  end
+
+  def rnd do 
+    0.0
+  end
+
+end
