6a273fe5a844b75017b47d835611aa5c81774deb
Max Sharples <maxsharples@gmail.com>
2014-06-11 20:57:32 +1000

    added placeholder functions

--- test/normal_distribution_test.exs
+++ test/normal_distribution_test.exs
@@ -1,4 +1,19 @@
 defmodule NormalDistributionTest do
   use ExUnit.Case, async: true
   doctest Statistics.Distributions.Normal
+
+  alias Statistics.Distributions.Normal, as: N
+
+  test "return a pmf function" do
+    assert N.pmf == 0.0
+  end
+
+  test "return a cdf function" do
+    assert N.cdf == 0.0
+  end
+
+  test "return a normally-distributed random number" do
+    assert N.rnd == 0.0
+  end
+
 end
