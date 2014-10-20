22b05dea1126845e0452a727f3a619c116b2a200
Max Sharples <maxsharples@gmail.com>
2014-10-20 22:02:53 +1100

    created the cdf function

--- test/normal_distribution_test.exs
+++ test/normal_distribution_test.exs
@@ -2,18 +2,21 @@ defmodule NormalDistributionTest do
   use ExUnit.Case, async: true
   doctest Statistics.Distributions.Normal
 
-  alias Statistics.Distributions.Normal, as: N
+  alias Statistics.Distributions.Normal, as: Norm
 
-  test "return a pmf function" do
-    assert N.pmf == 0.0
+  test "output of the pdf function" do
+    assert Norm.pdf(0) == 0.3989422804014327
+    assert Norm.pdf(3, 0.2, 1) == 0.00791545158297997
+    assert Norm.pdf(-1) == 0.24197072451914337
+    assert Norm.pdf(22.0, 23.5, 1.7) == 0.15900173884840293
   end
 
   test "return a cdf function" do
-    assert N.cdf == 0.0
+    assert Norm.cdf == 0.0
   end
 
   test "return a normally-distributed random number" do
-    assert N.rnd == 0.0
+    assert Norm.rnd == 0.0
   end
 
 end
