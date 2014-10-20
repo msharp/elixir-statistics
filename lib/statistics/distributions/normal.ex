82a68cceffa3966aebd45e21e98ff146ee047b23
Max Sharples <maxsharples@gmail.com>
2014-10-20 22:02:53 +1100

    created the cdf function

--- lib/statistics/distributions/normal.ex
+++ lib/statistics/distributions/normal.ex
@@ -4,16 +4,36 @@ defmodule Statistics.Distributions.Normal do
   import Statistics.MathHelpers
   alias Statistics.MathHelpers, as: M
 
-  def pmf do
-    0.0
+  @doc """
+  Probability density function
+  
+  get result of probability density function
+
+  ## Examples
+  
+      iex> Statistics.Distributions.Normal.pdf(0) 
+      0.3989422804014327
+      iex> Statistics.Distributions.Normal.pdf(1.3, 0.2, 1)
+      0.21785217703255055
+
+  """
+  def pdf(x) do
+    pdf(x, 0, 1)
+  end
+
+  def pdf(x, mu, sigma) do
+    numexp = M.pow((x - mu), 2) / (2 * M.pow(sigma, 2))
+    denom = sigma * M.sqrt((2 * M.pi))
+    numer = M.pow(M.e, (numexp * -1)) 
+    numer / denom
   end
 
   def cdf do
     0.0
   end
 
-  def rnd do 
-    0.0
-  end
+    def rnd do 
+      0.0
+    end
 
 end
