5d2e17347d0dfd171dfc6f8254d93c3a490fb617
Max Sharples <maxsharples@gmail.com>
2014-10-23 07:04:30 +1100

    moved all the functions from the Statistics.Descriptive module into Statistics module; Descriptive module removed

--- test/descriptive_test.exs
+++ test/descriptive_test.exs
@@ -1,106 +1,103 @@
 defmodule DescriptiveTest do
   use ExUnit.Case, async: true
-  doctest Statistics.Descriptive 
+  doctest Statistics
 
   test "sum a list" do
-    assert Statistics.Descriptive.sum([1,2,3,4,5,6,7,8,9]) == 45
+    assert Statistics.sum([1,2,3,4,5,6,7,8,9]) == 45
   end
 
   test "calculate mean" do
-    assert Statistics.Descriptive.mean([1]) == 1
-    assert Statistics.Descriptive.mean([1,2,3,4,5,6,7,8,9]) == 5
+    assert Statistics.mean([1]) == 1
+    assert Statistics.mean([1,2,3,4,5,6,7,8,9]) == 5
   end
 
   test "get mode" do 
-    assert Statistics.Descriptive.mode([1,2,3,2,4,5,2,6,7,2,8,9]) == 2
+    assert Statistics.mode([1,2,3,2,4,5,2,6,7,2,8,9]) == 2
   end
 
   test "calculate median" do
-    assert Statistics.Descriptive.median([1,2,3,4,5,6,7,8,9]) == 5
-    assert Statistics.Descriptive.median([1,2,3,4,5,6,7,8]) == 4.5
+    assert Statistics.median([1,2,3,4,5,6,7,8,9]) == 5
+    assert Statistics.median([1,2,3,4,5,6,7,8]) == 4.5
   end
 
   test "get maximum" do
-    assert Statistics.Descriptive.max([2,4,3,5,4,6,99,1,7,3,6]) == 99
+    assert Statistics.max([2,4,3,5,4,6,99,1,7,3,6]) == 99
   end
 
   test "get minimum" do
-    assert Statistics.Descriptive.min([23,45,34,53,44,65,99,1,74,32,69]) == 1
+    assert Statistics.min([23,45,34,53,44,65,99,1,74,32,69]) == 1
   end
 
   test "get first quartile point" do
-    assert Statistics.Descriptive.quartile([1,2,3,4,5,5,6,7,8,9],:first) == 3
-    assert Statistics.Descriptive.quartile([1,2,3,4,5,6,7,8,9],:first) == 3
+    assert Statistics.quartile([1,2,3,4,5,5,6,7,8,9],:first) == 3
+    assert Statistics.quartile([1,2,3,4,5,6,7,8,9],:first) == 3
   end
 
   test "get third quartile point" do
-    assert Statistics.Descriptive.quartile([1,2,3,4,5,5,6,7,8,9],:third) == 7
-    assert Statistics.Descriptive.quartile([1,2,3,4,5,6,7,8,9],:third) == 7
+    assert Statistics.quartile([1,2,3,4,5,5,6,7,8,9],:third) == 7
+    assert Statistics.quartile([1,2,3,4,5,6,7,8,9],:third) == 7
   end
 
   test "get nth percentile score" do
     a = [1,2,3,4,5,6,7,8,9]
-    assert Statistics.Descriptive.percentile(a,0) == 1
-    assert Statistics.Descriptive.percentile(a,20) == 2.6
-    assert Statistics.Descriptive.percentile(a,80) == 7.4
-    assert Statistics.Descriptive.percentile(a,100) == 9
+    assert Statistics.percentile(a,0) == 1
+    assert Statistics.percentile(a,20) == 2.6
+    assert Statistics.percentile(a,80) == 7.4
+    assert Statistics.percentile(a,100) == 9
   end
 
   test "get range" do
-    assert Statistics.Descriptive.range([1,2,3,4,5,6,7,8,9]) == 8
+    assert Statistics.range([1,2,3,4,5,6,7,8,9]) == 8
   end
 
   test "get inter-quartile range" do
-    assert Statistics.Descriptive.iqr([1,2,3,4,5,6,7,8,9]) == 4
+    assert Statistics.iqr([1,2,3,4,5,6,7,8,9]) == 4
   end
 
   test "calculate variance" do
-    assert Statistics.Descriptive.variance([4,3,3,4,5,6,7,6,5]) == 1.7283950617283952
+    assert Statistics.variance([4,3,3,4,5,6,7,6,5]) == 1.7283950617283952
   end
 
   test "calculate standard deviation" do
-    assert Statistics.Descriptive.stdev([4,3,3,4,5,6,7,6,5]) == 1.314684396244359
+    assert Statistics.stdev([4,3,3,4,5,6,7,6,5]) == 1.314684396244359
   end
 
   test "calculate trimmed mean" do 
-    assert Statistics.Descriptive.trimmed_mean([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]) == 8.0 # not trimmed
-    assert Statistics.Descriptive.trimmed_mean([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], {4,9}) == 6.5
-    assert Statistics.Descriptive.trimmed_mean([1,2,3,4,5,5,6,6,7,7,8,8,10,11,12,13,14,15],:iqr) == 7.3
+    assert Statistics.trimmed_mean([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]) == 8.0 # not trimmed
+    assert Statistics.trimmed_mean([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], {4,9}) == 6.5
+    assert Statistics.trimmed_mean([1,2,3,4,5,5,6,6,7,7,8,8,10,11,12,13,14,15],:iqr) == 7.3
   end
 
   test "calculate harmonic mean" do
-    assert Statistics.Descriptive.harmonic_mean([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]) == 4.5204836768674568
+    assert Statistics.harmonic_mean([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]) == 4.5204836768674568
   end
 
   test "calculate geometric mean" do
-    assert Statistics.Descriptive.geometric_mean([1,2,3,4,5,6]) == 2.9937951655239088
+    assert Statistics.geometric_mean([1,2,3,4,5,6]) == 2.9937951655239088
   end
 
   # moment/skew/kurtosis numbers match python/scipy
 
   test "calculate moment about the mean" do
     a = [1,2,3,4,5,6,7,8,9,8,7,6,5,4,3]
-    assert Statistics.Descriptive.moment(a,1) == 0.0
-    assert Statistics.Descriptive.moment(a,2) == 5.2266666666666675 
-    assert Statistics.Descriptive.moment(a,3) == -1.3440000000000025
+    assert Statistics.moment(a,1) == 0.0
+    assert Statistics.moment(a,2) == 5.2266666666666675 
+    assert Statistics.moment(a,3) == -1.3440000000000025
   end
 
   test "calculate skewness" do
-    assert Statistics.Descriptive.skew([1,2,3,2,1]) == 0.3436215967445454
+    assert Statistics.skew([1,2,3,2,1]) == 0.3436215967445454
   end
 
   test "calculate kurtosis (fisher)" do
-    assert Statistics.Descriptive.kurtosis([1,2,3,2,1]) == -1.1530612244897964
+    assert Statistics.kurtosis([1,2,3,2,1]) == -1.1530612244897964
   end
   
   test "calculate standard score for items in a list" do
-    #expected = [-0.74278135, -1.57841037, -0.74278135,
-    #            0.09284767, 0.92847669, 1.76410571,
-    #          0.92847669, 0.09284767, -0.74278135]
     expected =  [-0.7427813527082074, -1.5784103745049407, -0.7427813527082074,
                   0.09284766908852597, 0.9284766908852594, 1.7641057126819928,
                   0.9284766908852594, 0.09284766908852597, -0.7427813527082074]
-    assert Statistics.Descriptive.zscore([3,2,3,4,5,6,5,4,3]) == expected
+    assert Statistics.zscore([3,2,3,4,5,6,5,4,3]) == expected
   end
 
 end
