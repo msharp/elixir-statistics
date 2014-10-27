605663cffae6b464d57daa5f5cadff212d80073c
Max Sharples <maxsharples@gmail.com>
2014-10-27 22:10:02 +1100

    attemted cdf function - needs different beta function

--- lib/statistics/distributions/chisq.ex
+++ lib/statistics/distributions/chisq.ex
@@ -26,9 +26,19 @@ defmodule Statistics.Distributions.Chisq do
 
   @doc """
   The cumulative density function
+
+  ## Examples
+
+  #iex> Statistics.Distributions.Chisq(2, 2)
+  #    0.63212055882855778
+
+  http://rosettacode.org/wiki/Verify_distribution_uniformity/Chi-squared_test
+
   """
-  def cdf(x) do
-    0.0
+  def cdf(x, df) do
+    g = gamma(df/2) 
+    b = lgamma(df/2, x/2)
+    b / g  
   end
 
   @doc """
@@ -40,11 +50,27 @@ defmodule Statistics.Distributions.Chisq do
 
   @doc """
   Draw a random number from a t distribution with specified degrees of freedom
+
+  Uses the [rejection sampling method](https://en.wikipedia.org/wiki/Rejection_sampling)
+
+  ## Examples
+
+      iex> Statistics.Distributions.Chisq.rand(2)
+      1.232433646523534767
+
   """
   def rand(df) do
-    0.5
+    x = Math.rand() * 100
+    if pdf(x, df) > Math.rand() do
+      x
+    else
+      rand(df) # keep trying
+    end
   end
 
+  def lgamma(s, x) do
+    (s-1)*beta(s-1,x)-Math.pow(s, x-1)*Math.exp(-1*x)
+  end
 
   @doc """
   The Gamma function
--- test/chisq_distribution_test.exs
+++ test/chisq_distribution_test.exs
@@ -1,6 +1,6 @@
 defmodule ChisqDistributionTest do
   use ExUnit.Case, async: true
-  doctest Statistics.Distributions.Chisq, except: [rand: 0, rand: 3]
+  doctest Statistics.Distributions.Chisq, except: [rand: 1]
 
   alias Statistics.Distributions.Chisq, as: Chisq
   #alias Statistics.MathHelpers, as: Math
@@ -12,15 +12,23 @@ defmodule ChisqDistributionTest do
   end
 
   test "return a cdf " do
-    assert Chisq.cdf(0) == 0.0
+    # assert Chisq.cdf(2, 1) == 0.63212055882855778
   end
 
   test "return a random number from the distribution" do
     assert is_float Chisq.rand(2) 
+    #rands = for _ <- 0..10000, do: Chisq.rand(1)
+    #assert Statistics.mean(rands) == 1
   end
 
   test "get the percentile point value" do
     assert Chisq.ppf(0.0) == 0.0
+  end
+
+  test "beta function" do
+    assert Chisq.beta(1, 2) == 0.5
+    assert Chisq.beta(2, 2) == 0.1666666666666665
+    assert Chisq.beta(0.05, 1) == 19.999999999999996
   end
 
 end
