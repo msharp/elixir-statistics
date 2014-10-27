00db2e94c4f052dd613c1ce72f8c953b927f0363
Max Sharples <maxsharples@gmail.com>
2014-10-27 17:57:23 +1100

    more robust gamma function

--- lib/statistics/distributions/t.ex
+++ lib/statistics/distributions/t.ex
@@ -4,13 +4,26 @@ defmodule Statistics.Distributions.T do
 
   @moduledoc """
   Student's t distribution.
+
+  This distribution is always centered around 0.0 and allows a *degrees of freedom* parameter.
   """
 
   @doc """
   The probability density function
+
+  ## Examples
+
+      iex> Statistics.Distributions.T.pdf(0, 3)
+      0.36755259694786124
+      iex> Statistics.Distributions.T.pdf(3.2)
+      0.02831938489179633
+
   """
   def pdf(x, df) do
     gamma((df+1)/2) / (Math.sqrt(df*Math.pi) * gamma(df/2) ) * Math.pow((1 + (x*x/df)), ((df+1)/2)*-1)
+  end
+  def pdf(x) do
+    pdf(x, 1)
   end
 
   @doc """
@@ -31,7 +44,7 @@ defmodule Statistics.Distributions.T do
   Draw a random number from a t distribution with specified degrees of freedom
   """
   def rand(df) do
-    x = Math.rand() * 20 - 10
+    x = Math.rand() * 50 - 25 # t-dist is fatter-tailed than normal
     if pdf(x, df) > Math.rand() do
       x
     else
@@ -40,10 +53,39 @@ defmodule Statistics.Distributions.T do
   end
 
 
-  """
-  The gamma function
+  @doc """
+  The Gamma function
+
+  Two implementations available here:
+
+      - Using Taylor series coefficients 
+      - using the [Lanczos approximation](http://en.wikipedia.org/wiki/Lanczos_approximation)
+  
   """
   def gamma(x) do
+    gamma_lanczos(x)
+    #gamma_taylor(x)
+  end
+
+  defp gamma_lanczos(x) do
+    # coefficients used by the GNU Scientific Library
+    g = 7
+    p = [0.99999999999980993, 676.5203681218851, -1259.1392167224028,
+         771.32342877765313, -176.61502916214059, 12.507343278686905,
+         -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7]
+    # recursive formula
+    if x < 0.5 do
+      Math.pi / (:math.sin(Math.pi*x) * gamma_lanczos(1-x))
+    else
+      z = x - 1
+      xs = for i <- 1..8, do: Enum.at(p, i)/(z+i)
+      x = Enum.at(p, 0) + Enum.sum(xs)
+      t = z + g + 0.5
+      Math.sqrt(2*Math.pi) * Math.pow(t, (z+0.5)) * Math.exp(-1*t) * x
+    end
+  end
+
+  defp gamma_taylor(x) do
     a = [1.00000000000000000000, 0.57721566490153286061, -0.65587807152025388108,
         -0.04200263503409523553, 0.16653861138229148950, -0.04219773455554433675,
         -0.00962197152787697356, 0.00721894324666309954, -0.00116516759185906511,
@@ -56,13 +98,22 @@ defmodule Statistics.Distributions.T do
         0.00000000000000000141, -0.00000000000000000023] # , 0.00000000000000000002]
 
     sm = 0.00000000000000000002
-    1.0 / gamma_map(x-1.0, Enum.reverse(a), sm)
+    1.0 / gamma_taylor_map(x-1.0, Enum.reverse(a), sm)
   end
-  defp gamma_map(y, [], acc) do
+  defp gamma_taylor_map(y, [], acc) do
     acc
   end
-  defp gamma_map(y, [h|t], acc) do
-    gamma_map(y, t, acc * y + h)
+  defp gamma_taylor_map(y, [h|t], acc) do
+    gamma_taylor_map(y, t, acc * y + h)
+  end
+
+
+  @doc """
+  The Beta function
+  """
+  def beta(x, y) do
+    # from https://en.wikipedia.org/wiki/Beta_function#Properties
+    gamma(x) * gamma(y) / gamma(x + y)
   end
 
 end
--- test/t_distribution_test.exs
+++ test/t_distribution_test.exs
@@ -7,8 +7,9 @@ defmodule TDistributionTest do
 
 
   test "output of the pdf function" do
-    assert T.pdf(0, 3) == 0.36755259694786135
-    assert T.pdf(0.1, 7) == 0.3827987876130386
+    assert T.pdf(0, 3) == 0.36755259694786124
+    assert T.pdf(0.1, 7) ==  0.38279933426055135
+    assert T.pdf(0.1, 77) == 0.39564030492250557
   end
 
   test "return a cdf " do
@@ -18,7 +19,7 @@ defmodule TDistributionTest do
   test "return a random number from the distribution" do
     assert is_float T.rand(2) 
     # rands = for _ <- 0..10000, do: T.rand(3)
-    # assert T.rand(77) == 0.5
+    #assert T.rand(77) == 0.5
   end
 
   test "get the percentile point value" do
