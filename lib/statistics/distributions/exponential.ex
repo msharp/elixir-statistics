bff732ade1ad7b63721433c950cb4e39053f4349
Max Sharples <maxsharples@gmail.com>
2015-05-25 17:22:36 +1000

    exponential distribution

---
+++ lib/statistics/distributions/exponential.ex
@@ -0,0 +1,96 @@
+defmodule Statistics.Distributions.Exponential do
+  @moduledoc """
+  Exponential distribution.
+
+  `lambda` is the rate parameter and must be greater than zero. 
+  """
+
+  alias Statistics.Math
+
+  @doc """
+  The probability density function
+
+  ## Examples
+
+      iex> Statistics.Distributions.Exponential.pdf(1, 1)
+      0.36787944117144233
+
+  """
+  def pdf(x) do
+    pdf(x, 1)
+  end
+  def pdf(x, _) when x < 0 do
+    0
+  end
+  def pdf(_, lambda) when lambda <= 0 do
+    :nan
+  end
+  def pdf(x, lambda) do
+    lambda * Math.exp(-lambda*x)
+  end
+
+  @doc """
+  The cumulative density function
+
+  ## Examples
+
+      iex> Statistics.Distributions.Exponential.cdf(1, 1)
+      0.6321205588285577
+
+  """
+  def cdf(x) do
+    cdf(x, 1)
+  end
+  def cdf(x, _) when x < 0 do
+    0
+  end
+  def cdf(_, lambda) when lambda <= 0 do
+    :nan
+  end
+  def cdf(x, lambda) do
+    1 - Math.exp(-lambda*x)
+  end
+
+  @doc """
+  The percentile-point function
+
+  ## Examples
+
+      iex> Statistics.Distributions.Exponential.ppf(0.1)
+      0.10536051565782628
+
+  """
+  def ppf(x) do
+    ppf(x, 1)
+  end
+  def ppf(x, _) when x == 1 do
+    :inf
+  end
+  def ppf(x, lambda) when x < 0 or x > 1 or lambda < 0 do
+    :nan
+  end
+  def ppf(x, lambda) do
+    -1 * Math.ln(1-x) / lambda
+  end
+  
+  @doc """
+  Draw a random number from the distribution with specified lambda
+
+  Uses the [rejection sampling method](https://en.wikipedia.org/wiki/Rejection_sampling)
+
+  ## Examples
+
+      iex> Statistics.Distributions.Exponential.rand(1)
+      0.145709384787
+
+  """
+  def rand(lambda) do
+    x = Math.rand() * lambda * 100 
+    if pdf(x, lambda) > Math.rand() do
+      x
+    else
+      rand(lambda) # keep trying
+    end
+  end
+
+end
---
+++ test/exponential_distribution_test.exs
@@ -0,0 +1,39 @@
+defmodule ExponentialDistributionTest do
+  use ExUnit.Case, async: true
+  doctest Statistics.Distributions.Exponential, except: [rand: 1]
+
+  alias Statistics.Distributions.Exponential
+
+
+  test "output of the pdf function" do
+    assert Exponential.pdf(-1) == 0
+    assert Exponential.pdf(1, 0) == :nan
+    assert Exponential.pdf(1, -1) == :nan
+    assert Exponential.pdf(1) == 0.36787944117144233
+    assert Exponential.pdf(2, 3) == 0.0074362565299990755
+    assert Exponential.pdf(9, 2) == 3.0459959489425258e-08
+  end
+
+  test "return a cdf " do
+    assert Exponential.cdf(-1) == 0
+    assert Exponential.cdf(1, 0) == :nan
+    assert Exponential.cdf(1, -1) == :nan
+    assert Exponential.cdf(1, 1) == 0.63212055882855767
+    assert Exponential.cdf(2, 3) == 0.9975212478233336
+    assert Exponential.cdf(9, 2) == 0.99999998477002028
+  end
+
+  test "return a random number from the distribution" do
+    assert is_float Exponential.rand(2)
+  end
+
+  test "get the percentile point value" do
+    assert Exponential.ppf(-1) == :nan
+    assert Exponential.ppf(1.2) == :nan
+    assert Exponential.ppf(0) == 0
+    assert Exponential.ppf(1) == :inf
+    assert Exponential.ppf(0.5) == 0.6931471805599453
+    assert Exponential.ppf(0.9, 4) == 0.57564627324851148
+  end
+
+end
