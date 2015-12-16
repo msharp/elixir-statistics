4d17187783fd9a4dbdcd936aa64ce89668786155
Max Sharples <maxsharples@gmail.com>
2015-12-16 12:02:25 +1100

    exponential distribution functions

--- lib/statistics/distributions/exponential.ex
+++ lib/statistics/distributions/exponential.ex
@@ -12,21 +12,24 @@ defmodule Statistics.Distributions.Exponential do
 
   ## Examples
 
-      iex> Statistics.Distributions.Exponential.pdf(1, 1)
+      iex> Statistics.Distributions.Exponential.pdf().(1)
       0.36787944117144233
 
   """
-  def pdf(x) do
-    pdf(x, 1)
+  def pdf() do
+    pdf(1)
   end
-  def pdf(x, _) when x < 0 do
-    0
-  end
-  def pdf(_, lambda) when lambda <= 0 do
-    :nan
-  end
-  def pdf(x, lambda) do
-    lambda * Math.exp(-lambda*x)
+  def pdf(lambda) do
+    fn x ->
+      cond do
+        x < 0 -> 
+          0
+        lambda <= 0 ->  
+          :nan
+        true ->
+          lambda * Math.exp(-lambda*x)
+      end
+    end
   end
 
   @doc """
@@ -34,21 +37,24 @@ defmodule Statistics.Distributions.Exponential do
 
   ## Examples
 
-      iex> Statistics.Distributions.Exponential.cdf(1, 1)
+      iex> Statistics.Distributions.Exponential.cdf().(1)
       0.6321205588285577
 
   """
-  def cdf(x) do
-    cdf(x, 1)
+  def cdf() do
+    cdf(1)
   end
-  def cdf(x, _) when x < 0 do
-    0
-  end
-  def cdf(_, lambda) when lambda <= 0 do
-    :nan
-  end
-  def cdf(x, lambda) do
-    1 - Math.exp(-lambda*x)
+  def cdf(lambda) do
+    fn x ->
+      cond do 
+        x < 0 ->
+          0
+        lambda <= 0 ->
+          :nan
+        true ->
+          1 - Math.exp(-lambda*x)
+      end
+    end
   end
 
   @doc """
@@ -56,21 +62,24 @@ defmodule Statistics.Distributions.Exponential do
 
   ## Examples
 
-      iex> Statistics.Distributions.Exponential.ppf(0.1)
+      iex> Statistics.Distributions.Exponential.ppf().(0.1)
       0.10536051565782628
 
   """
-  def ppf(x) do
-    ppf(x, 1)
+  def ppf() do
+    ppf(1)
   end
-  def ppf(x, _) when x == 1 do
-    :inf
-  end
-  def ppf(x, lambda) when x < 0 or x > 1 or lambda < 0 do
-    :nan
-  end
-  def ppf(x, lambda) do
-    -1 * Math.ln(1-x) / lambda
+  def ppf(lambda) do
+    fn x ->
+      cond do 
+        x == 1 ->
+          :inf
+        x < 0 or x > 1 or lambda < 0 ->
+          :nan
+        true ->
+          -1 * Math.ln(1-x) / lambda
+      end
+    end
   end
 
   @doc """
@@ -86,7 +95,7 @@ defmodule Statistics.Distributions.Exponential do
   """
   def rand(lambda) do
     x = Math.rand() * lambda * 100
-    if pdf(x, lambda) > Math.rand() do
+    if pdf(lambda).(x) > Math.rand() do
       x
     else
       rand(lambda) # keep trying
--- test/exponential_distribution_test.exs
+++ test/exponential_distribution_test.exs
@@ -6,21 +6,21 @@ defmodule ExponentialDistributionTest do
 
 
   test "output of the pdf function" do
-    assert Exponential.pdf(-1) == 0
-    assert Exponential.pdf(1, 0) == :nan
-    assert Exponential.pdf(1, -1) == :nan
-    assert Exponential.pdf(1) == 0.36787944117144233
-    assert Exponential.pdf(2, 3) == 0.0074362565299990755
-    assert Exponential.pdf(9, 2) == 3.0459959489425258e-08
+    assert Exponential.pdf().(-1) == 0
+    assert Exponential.pdf(0).(1) == :nan
+    assert Exponential.pdf(-1).(1) == :nan
+    assert Exponential.pdf().(1) == 0.36787944117144233
+    assert Exponential.pdf(3).(2) == 0.0074362565299990755
+    assert Exponential.pdf(2).(9) == 3.0459959489425258e-08
   end
 
   test "return a cdf " do
-    assert Exponential.cdf(-1) == 0
-    assert Exponential.cdf(1, 0) == :nan
-    assert Exponential.cdf(1, -1) == :nan
-    assert Exponential.cdf(1, 1) == 0.63212055882855767
-    assert Exponential.cdf(2, 3) == 0.9975212478233336
-    assert Exponential.cdf(9, 2) == 0.99999998477002028
+    assert Exponential.cdf().(-1) == 0
+    assert Exponential.cdf(0).(1) == :nan
+    assert Exponential.cdf(-1).(1) == :nan
+    assert Exponential.cdf().(1) == 0.63212055882855767
+    assert Exponential.cdf(3).(2) == 0.9975212478233336
+    assert Exponential.cdf(2).(9) == 0.99999998477002028
   end
 
   test "return a random number from the distribution" do
@@ -28,12 +28,12 @@ defmodule ExponentialDistributionTest do
   end
 
   test "get the percentile point value" do
-    assert Exponential.ppf(-1) == :nan
-    assert Exponential.ppf(1.2) == :nan
-    assert Exponential.ppf(0) == 0
-    assert Exponential.ppf(1) == :inf
-    assert Exponential.ppf(0.5) == 0.6931471805599453
-    assert Exponential.ppf(0.9, 4) == 0.57564627324851148
+    assert Exponential.ppf().(-1) == :nan
+    assert Exponential.ppf(1).(1.2) == :nan
+    assert Exponential.ppf().(0) == 0
+    assert Exponential.ppf(1).(1) == :inf
+    assert Exponential.ppf(1).(0.5) == 0.6931471805599453
+    assert Exponential.ppf(4).(0.9) == 0.57564627324851148
   end
 
 end
